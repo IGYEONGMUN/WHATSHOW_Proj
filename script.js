ab8935b8bf4e9261f17c9e4875739184e97ca7ec
Myeonkwan72 <audrhks072@gmail.com>
2025-02-22 20:40:56 +0900

    myeongkwan 250222 commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const hover = document.querySelectorAll(".hover");
+console.log(hover);
+
+hover.addEventListener("mouseenter", () => {
+  console.log("ok");
+});
+
+hover.addEventListener("mouseleave", () => {
+  console.log("ok");
+});
