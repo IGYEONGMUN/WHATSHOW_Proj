c04084c9ecc3cc0edc46764cd212dc7e945b3922
IGYEONGMUN <doehdotn2@naver.com>
2025-02-27 01:49:02 +0900

    250227 gyeoungmun commit

--- event_gyeoungmun/event_1.js
+++ event_gyeoungmun/event_1.js
@@ -1,5 +1,6 @@
-const h1_1 = document.querySelector(".h1_1");
-const h1_2 = document.querySelector(".h1_2");
+const p1 = document.querySelector(".p_1");
+const p2 = document.querySelector(".p_2");
+const p4 = document.querySelector(".p4");
 const h1_3 = document.querySelector(".h1_3");
 const check1 = document.querySelector("#check1");
 const check2 = document.querySelector("#check2");
@@ -7,60 +8,73 @@ const check3 = document.querySelector("#check3");
 const check4 = document.querySelector("#check4");
 const h4 = document.querySelector(".h4");
 const main = document.querySelector(".lgm_main_section");
+const rotateImg = document.querySelector(".lgm_main_section");
+const rotateDesc = document.querySelector(".desc");
+const mainInput = document.querySelector(".main_input");
 
-function h1func() {
+function p1func() {
   setTimeout(() => {
-    h1_1.style.opacity = "1";
-  }, 1000);
+    p1.style.opacity = "1";
+  }, 1200);
 }
-function h1func() {
+
+function p2func() {
   setTimeout(() => {
-    h1_1.style.opacity = "1";
-  }, 1000);
-}
-function h2func() {
-  setTimeout(() => {
-    h1_2.style.opacity = "1";
-  }, 2300);
+    p2.style.opacity = "1";
+  }, 3900);
 }
 function h3func() {
   setTimeout(() => {
     h1_3.style.opacity = "1";
-  }, 3000);
+  }, 5500);
 }
 
 function checkone() {
   setTimeout(() => {
     check1.style.opacity = "1";
-  }, 1300);
+  }, 2500);
 }
 function checktwo() {
   setTimeout(() => {
     check2.style.opacity = "1";
-  }, 1500);
+  }, 2700);
 }
 function checkthree() {
   setTimeout(() => {
     check3.style.opacity = "1";
-  }, 1700);
+  }, 2900);
 }
 function checkfour() {
   setTimeout(() => {
     check4.style.opacity = "1";
-  }, 1900);
+  }, 3100);
 }
 
-function h4func() {
+function p4func() {
   setTimeout(() => {
-    h4.style.opacity = "1";
-  }, 3900);
+    p4.style.opacity = "1";
+  }, 7200);
 }
 
-h1func();
-h2func();
+p1func();
+p2func();
 h3func();
 checkone();
 checktwo();
 checkthree();
 checkfour();
-h4func();
+p4func();
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 350) {
+    rotateImg.classList.add("rotate");
+    rotateImg.style.opacity = "1";
+    setTimeout(() => {
+      rotateDesc.style.opacity = "1";
+    }, 4000);
+
+    setTimeout(() => {
+      mainInput.style.opacity = "1";
+    }, 7500);
+  }
+});
